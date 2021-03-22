ff11a27dc4fcc7f73b8208d279aa9e7a86c59061
Megumin-Master <60296660+Megumin-Master@users.noreply.github.com>
2021-03-22 10:57:56 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+  * [HelloWorld.java]
+  * A sample program to test compilation of code
+  * Author:  Ms. Andrighetti
+  * Date: September 10, 2015
+  */
+class HelloWorld {
+	public static void main(String args[]) {
+    String greet = "Hello World";
+    
+		System.out.println(greet); // output a message
+    System.out.println("This is a line I'm uploading to GitHub");
+    // "staging" a file is called "commiting"
+    //Uploading the file to the repository is called "push"
+	}
+}
